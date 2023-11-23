f599310ba144370fa7bf7a5fbdb91736ed5232a7
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-23 17:46:29 +0100

    Add chat page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/chat" element={<Chat />} />
             {/* <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
              */}
