4c968a3ab5f9ae92f450d211b2f88da82e1aaf2a
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-23 09:59:06 +0100

    Add hero to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>
-}
+  return (
+    <div>
+      <section className="mx-auto flex max-w-6xl flex-col bg-white px-4 pb-20 pt-16 sm:px-6 lg:px-8">
+        <div className="mx-auto flex max-w-2xl flex-col items-center justify-center text-center">
+          <h1 className="text-4xl text-gray-800 md:text-6xl">
+            Navigate Your Health Journey with AI Wisdom
+          </h1>
+          <p className="pt-6 text-xl font-light text-gray-400">
+            Introducing SymptoSage, your AI companion for instant symptom
+            insights and medical guidance. Unlock the power of AI-driven
+            conversations, providing you with rapid responses and informed
+            suggestions. Your virtual health advisor is just a chat away! Trust
+            the future of healthcare, tailored to you, in the palm of your hand.
+          </p>
+        </div>
 
-export default Home
+        <Link className="bg mx-auto mt-8 flex items-center justify-center rounded-md bg-sky-500 px-6 py-3 text-lg font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">
+          Get Started
+        </Link>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
