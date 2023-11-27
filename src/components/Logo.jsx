09ce55e59a013c97b4f988aa5093823e35ffb82b
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-11-27 14:29:36 +0100

    modify logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,13 @@
+import { FaLaptopMedical } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <Link to="/">
-      <span className="font-lobster text-4xl text-sky-900">AskDoc</span>
+      <span className="flex items-center gap-2 font-lobster text-4xl text-sky-900">
+        AskDoc
+        <FaLaptopMedical size={"1em"} />
+      </span>{" "}
     </Link>
   );
 };
